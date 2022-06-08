14f447ab3249258d34ddc4ffb3920ebb5afcdc8e
MihaiDavid05 <44817698+MihaiDavid05@users.noreply.github.com>
2022-06-09 00:59:14 +0200

    Another experiment and anither paper read

--- utils/network.py
+++ utils/network.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 # REF: https://github.com/geifmany/cifar-vgg/blob/master/cifar10vgg.py
 class VGG16CIFAR(nn.Module):
-    def __init__(self, n_classes):
+    def __init__(self, n_classes, init_weights=True):
         super(VGG16CIFAR, self).__init__()
         # TODO: Maybe delete Batchnorm, or see momentum and alpha/beta init values as in Facebook paper (but harder)
         self.n_classes = n_classes
@@ -12,69 +12,86 @@ class VGG16CIFAR(nn.Module):
         self.model = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=(3, 3), padding='same'),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(64),
+            nn.BatchNorm2d(64, momentum=0.99),
             nn.Dropout(0.3),
             nn.Conv2d(64, 64, kernel_size=(3, 3), padding='same'),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(64),
+            nn.BatchNorm2d(64, momentum=0.99),
             nn.MaxPool2d(2),
 
             nn.Conv2d(64, 128, kernel_size=(3, 3), padding='same'),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(128),
+            nn.BatchNorm2d(128, momentum=0.99),
             nn.Dropout(0.4),
             nn.Conv2d(128, 128, kernel_size=(3, 3), padding='same'),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(128),
+            nn.BatchNorm2d(128, momentum=0.99),
             nn.MaxPool2d(2),
 
             nn.Conv2d(128, 256, kernel_size=(3, 3), padding='same'),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(256),
+            nn.BatchNorm2d(256, momentum=0.99),
             nn.Dropout(0.4),
             nn.Conv2d(256, 256, kernel_size=(3, 3), padding='same'),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(256),
+            nn.BatchNorm2d(256, momentum=0.99),
             nn.Dropout(0.4),
             nn.Conv2d(256, 256, kernel_size=(3, 3), padding='same'),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(256),
+            nn.BatchNorm2d(256, momentum=0.99),
             nn.MaxPool2d(2),
 
             nn.Conv2d(256, 512, kernel_size=(3, 3), padding='same'),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(512),
+            nn.BatchNorm2d(512, momentum=0.99),
             nn.Dropout(0.4),
             nn.Conv2d(512, 512, kernel_size=(3, 3), padding='same'),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(512),
+            nn.BatchNorm2d(512, momentum=0.99),
             nn.Dropout(0.4),
             nn.Conv2d(512, 512, kernel_size=(3, 3), padding='same'),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(512),
+            nn.BatchNorm2d(512, momentum=0.99),
             nn.MaxPool2d(2),
 
             nn.Conv2d(512, 512, kernel_size=(3, 3), padding='same'),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(512),
+            nn.BatchNorm2d(512, momentum=0.99),
             nn.Dropout(0.4),
             nn.Conv2d(512, 512, kernel_size=(3, 3), padding='same'),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(512),
+            nn.BatchNorm2d(512, momentum=0.99),
             nn.Dropout(0.4),
             nn.Conv2d(512, 512, kernel_size=(3, 3), padding='same'),
             nn.ReLU(inplace=True),
-            nn.BatchNorm2d(512),
+            nn.BatchNorm2d(512, momentum=0.99),
             nn.MaxPool2d(2),
 
             nn.Dropout(0.5),
             nn.Flatten(),
             nn.Linear(512, 512),
             nn.ReLU(inplace=True),
-            nn.BatchNorm1d(512),
+            # TODO: Check this
+            # nn.BatchNorm1d(512, momentum=0.99),
             nn.Dropout(0.5),
             nn.Linear(512, self.n_classes)
         )
+
+        if init_weights:
+            self._initialize_weights()
+
+    def _initialize_weights(self) -> None:
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
         logits = self.model(x)
