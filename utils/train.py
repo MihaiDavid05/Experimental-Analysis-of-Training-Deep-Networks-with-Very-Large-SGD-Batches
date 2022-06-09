e62094294419ccfb136d543fe59ae44bb4bda7c4
MihaiDavid05 <44817698+MihaiDavid05@users.noreply.github.com>
2022-06-09 21:44:45 +0200

    Changed gradual warmuo

--- utils/train.py
+++ utils/train.py
@@ -57,7 +57,7 @@ def train(dataset, net, config, writer, device='cpu'):
         scheduler = optim.lr_scheduler.CyclicLR(optimizer, base_lr=0.01, max_lr=lr)
     elif use_lr_scheduler == 'GradualWarmup':
         scheduler_multisteplr = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30, 60, 90], gamma=0.1)
-        scheduler = WarmupLR(scheduler_multisteplr, init_lr=0.001, num_warmup=5, warmup_strategy='linear')
+        scheduler = WarmupLR(scheduler_multisteplr, init_lr=0.01, num_warmup=5, warmup_strategy='linear')
         # TODO: check init learning rate here
     elif use_lr_scheduler != 0:
         raise KeyError("LR scheduler not properly set !")
