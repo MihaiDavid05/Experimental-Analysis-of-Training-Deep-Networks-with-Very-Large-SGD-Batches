2148a32d8a2f3e274a8ab88cd8e6a87ca9f772fa
MihaiDavid05 <44817698+MihaiDavid05@users.noreply.github.com>
2022-06-10 16:38:53 +0200

    exp4 bash

---
+++ run_exp4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=vgg13_exp4
+#SBATCH --output=vgg13_exp4_%A.log
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --mem=24G
+#SBATCH --time=96:00:00
+#SBATCH --partition=gpu
+
+module purge
+module load CUDA/11.1.1-GCC-10.2.0
+module load cuDNN/8.0.5.39-CUDA-11.1.1
+module load CUDAcore/11.1.1
+
+python3 experiments.py 4
